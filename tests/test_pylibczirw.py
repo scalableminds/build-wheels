84ec2e40ac231ae734708588adb7c3adddb1cf1a
brokkoli71 <brokkoli71b@gmail.com>
2024-06-11 11:35:35 +0200

    download testdata

--- tests/test_pylibczirw.py
+++ tests/test_pylibczirw.py
@@ -1,9 +1,14 @@
 from pylibCZIrw import czi as pyczi
+import requests
 
+def test_pylibczirw():
 
-def test_pylibczirw(czipath):
-    # open the CZI document to read the
-    with pyczi.open_czi(czipath) as czidoc:
+    url = "https://github.com/zeiss-microscopy/OAD/blob/3b5d5e595707f1a7110ccd524b6adb10ccea65cd/Testdata/Translocation_comb_96_5ms.czi"
+    r = requests.get(url)
+    with open("testdata.czi",'wb') as f:
+        f.write(r.content)
+
+    with pyczi.open_czi("testdata.czi") as czidoc:
         # get the image dimensions as a dictionary, where the key identifies the dimension
         total_bounding_box = czidoc.total_bounding_box
 
